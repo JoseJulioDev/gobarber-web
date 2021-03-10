f17579213f148a9f68308ce2f1509e38246a7e99
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2021-03-10 19:39:42 -0300

    adding registration validation using Yup

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import {Form} from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,9 +12,21 @@ import { Container, Content,  Background } from './styles';
 
 const SignUp: React.FC = () => {
 
-    function handleSubmit(data: object): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async(data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                nome: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+                senha: Yup.string().min(6 , "senha no minimo 6 dígitos")
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
 
     return(
         <Container>
