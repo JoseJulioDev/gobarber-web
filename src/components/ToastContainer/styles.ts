59f7488e5c9e5c2f20554ae5e3f83adaecb713ff
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2021-03-25 22:54:29 -0300

    adding and removing toast

--- src/components/ToastContainer/styles.ts
+++ src/components/ToastContainer/styles.ts
@@ -1,9 +1,4 @@
-import styled, {css} from 'styled-components';
-
-interface ToastProps {
-    type?: 'success' | 'error' | 'info';
-    hasDescription: boolean;
-}
+import styled from 'styled-components';
 
 export const Container = styled.div `
     position: absolute;
@@ -11,69 +6,4 @@ export const Container = styled.div `
     top: 0;
     padding: 30px;
     overflow: hidden;
-`;
-
-const toastTypeVariations = {
-    info: css`
-        background: #EBF8FF;
-        color: #3172B7;
-    `,
-    success: css`
-        background: #E6FFFA;
-        color: #2E656A;
-    `,
-    error: css`
-        background: #FDDEDE;
-        color: #C53030;
-    `
-}
-
-export const Toast = styled.div<ToastProps>`
-    width: 360px;
-    position: relative;
-    padding: 16px 30px 16px 16px;
-    border-radius: 10px;
-    box-shadow: 2ox 2px 8px rgb(0, 0 , 0, 0.2);
-    
-    display: flex;
-
-    & + div {
-        margin-top: 8px;
-    }
-
-    ${props => toastTypeVariations[props.type || "info"]}
-
-    > svg {
-        margin: 4px 12px 0 0;
-    }
-
-    div {
-        flex: 1;
-
-        p {
-            margin-top: 4px;
-            font-size: 14px;
-            opacity: 0.8;
-            line-height: 20px;
-        }
-    }
-
-    button {
-        position: absolute;
-        right: 16px;
-        top: 19px;
-        opacity: 0.5;
-        border: 0;
-        background: transparent;
-        color: inherit;
-    }
-
-    ${props => !props.hasDescription && css`
-        align-items: center;
-
-        svg {
-            margin-top: 0;
-        }
-    `}
-
 `;
