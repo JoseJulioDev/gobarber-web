6da3be49e063266e20303d62a9e9e899b8f682da
JoseJulioDev <josejulioumbelino.dev@gmail.com>
2021-03-31 20:27:14 -0300

    finalizing user registration, adding toast and storing routes in history

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,25 +3,38 @@ import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import {Form} from '@unform/web';
 import * as Yup from 'yup';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
+
+import api from '../../services/api';
 
 import getValidationErros from '../../utils/getValidationErros';
 import logoImg from '../../assets/logo.svg';
+
+import {useToast} from '../../hooks/toast';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
 
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const {addToast} = useToast();
+    const history = useHistory();
+
+    const handleSubmit = useCallback(async (data: SignUpFormData) => {
         try {
             formRef.current?.setErrors({});
 
             const schema = Yup.object().shape({
-                nome: Yup.string().required('Nome obrigatório'),
+                name: Yup.string().required('Nome obrigatório'),
                 email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
                 password: Yup.string().min(6 , 'senha no minimo 6 dígitos')
             });
@@ -29,11 +42,30 @@ const SignUp: React.FC = () => {
             await schema.validate(data, {
                 abortEarly: false,
             });
+
+            await api.post('/users', data);
+
+            history.push('/');
+
+            addToast({
+                type: 'success',
+                title: 'Cadastro realizado',
+                description: 'Você já pode fazer o seu logon no GoBarber!'
+            });
         } catch (err) {
-            const erros = getValidationErros(err);
-            formRef.current?.setErrors(erros);
+            if(err instanceof Yup.ValidationError) {
+                const erros = getValidationErros(err);
+                formRef.current?.setErrors(erros);
+                return;
+            }
+            
+            addToast({
+                type: 'error',
+                title: 'Erro no cadastro',
+                description: 'Ocorreu um erro ao fazer cadastro, teste novamente'
+            });
         }
-    }, []);
+    }, [addToast, history]);
 
     return(
         <Container>
@@ -43,7 +75,7 @@ const SignUp: React.FC = () => {
                     <img src={logoImg} alt="GoBarber"/>
                     <Form ref={formRef} onSubmit={handleSubmit}>
                         <h1>Faça seu Cadastro</h1>
-                        <Input name="nome" icon={FiUser} placeholder="Nome" />
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
                         <Input name="email" icon={FiMail} placeholder="E-mail" />
                         <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
                         <Button type="submit">Cadastrar</Button>
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 import {useAuth} from '../../hooks/auth';
 import {useToast} from '../../hooks/toast';
@@ -26,6 +26,7 @@ const SignIn: React.FC = () => {
 
     const { signIn } = useAuth();
     const { addToast } = useToast();
+    const history = useHistory();
 
     const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
@@ -45,6 +46,7 @@ const SignIn: React.FC = () => {
                password: data.password
            });
 
+           history.push('/dashboard');
         } catch(err) {
             if(err instanceof Yup.ValidationError) {
                 const erros = getValidationErros(err);
@@ -58,7 +60,7 @@ const SignIn: React.FC = () => {
                 description: 'Ocorreu um erro ao fazer login cheque as credenciais.'
             });
         }
-    }, [signIn, addToast]);
+    }, [signIn, addToast, history]);
 
     return (
         <Container>
